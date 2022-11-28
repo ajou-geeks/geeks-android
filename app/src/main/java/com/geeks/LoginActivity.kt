4470f74c07b0bfd11890757f000c807e0da657c3
minseok baek <white 0825@ajou.ac.kr>
2022-11-28 20:32:03 +0900

    fix

--- app/src/main/java/com/geeks/LoginActivity.kt
+++ app/src/main/java/com/geeks/LoginActivity.kt
@@ -3,19 +3,32 @@ package com.geeks
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.geeks.databinding.ActivityLoginBinding
 import com.geeks.home.HomepageActivity
 import com.google.android.material.button.MaterialButton
 
 class LoginActivity : AppCompatActivity() {
+    private var _binding: ActivityLoginBinding?=null
+
+    private val binding get()=_binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val loginButton=findViewById<MaterialButton>(R.id.loginButton)
+        _binding=ActivityLoginBinding.inflate(layoutInflater)
 
-        loginButton.setOnClickListener {
+        binding.loginButton.setOnClickListener {
             val intent = Intent(this, HomepageActivity::class.java)
             startActivity(intent)
         }
+
+        binding.registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        val view=binding.root
+        setContentView(view)
     }
 }
