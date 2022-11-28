ebfb4abd9af5f46eef0743221fc3d58be8c19573
minseok baek <white 0825@ajou.ac.kr>
2022-11-28 11:44:13 +0900

    List added

---
+++ app/src/main/java/com/geeks/group/delivery/DeliveryListActivity.kt
@@ -0,0 +1,10 @@
+package com.geeks.group.delivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeliveryListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/geeks/group/product/ProductListActivity.kt
@@ -0,0 +1,10 @@
+package com.geeks.group.product
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
