3ef1f04d9cb15eec88eeca9f62115099339d9b60
minseok baek <white 0825@ajou.ac.kr>
2022-11-29 19:22:39 +0900

    feed added

---
+++ app/src/main/java/com/geeks/group/taxi/TaxiFeedActivity.kt
@@ -0,0 +1,12 @@
+package com.geeks.group.taxi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geeks.R
+
+class TaxiFeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed)
+    }
+}
